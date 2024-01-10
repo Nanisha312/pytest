28ab26b279a958c5725ddaae8f0cf0ed7458742e
Nanisha312 <nanishanani90@gmail.com>
2024-01-10 11:41:02 +0530

    add pytest

---
+++ test_math1_func.py
@@ -0,0 +1,12 @@
+import math1_func
+import pytest
+
+@pytest.mark.parametrize('num1, num2, result',
+                         [
+                             (7, 3, 10),
+                             ('Hello', ' World', 'Hello World'),
+                             (10.5, 25.5, 36)
+                         ]
+                         )
+def test_add(num1, num2, result):
+    assert math1_func.add(num1, num2) == result
